be8c2e889d05aad2f50ba86dc0ba23708fb6d2d1
sai prakash <saiprakashpagadala95@gmail.com>
2022-03-17 11:18:18 +0530

    location changes

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Location from './Components/Location/location';
+
+function App() {
+  return (
+    <div className="App">
+      <Location />
+    </div>
+  );
+}
+
+export default App;
